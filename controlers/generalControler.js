948935c616ec54f66b50796c33a1774b9b7ab2b2
jgomezacin1969 <jgomezacin@gmail.com>
2020-02-04 22:49:37 +0100

    1

--- controlers/generalControler.js
+++ controlers/generalControler.js
@@ -8,7 +8,7 @@ function nosotros(req,res){
 };
 
 function nuevo_proyecto(req,res){
-    res.render('nosotros.pug',{titulo:'Nuevo proyecto', variable01:'Variable 01'})
+    res.render('nuevo_proyecto.pug',{titulo:'Nuevo proyecto', variable01:'Variable 01'})
 };
 
 module.exports = {principal,nosotros, nuevo_proyecto};

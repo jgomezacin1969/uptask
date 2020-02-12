66db7e8aca01eab74572e831242faa3f30d95836
Gomez Acin <juagomez@t-systems.es>
2020-02-13 00:19:02 +0100

    d13

--- controlers/generalControler.js
+++ controlers/generalControler.js
@@ -1,3 +1,5 @@
+const Proyectos = require('../models/Proyectos');
+
 function principal(req, res) {
     res.render('principal.pug', { titulo: 'Pagina Principal', variable01: 'Variable 01' });
 }
@@ -11,10 +13,24 @@ function nuevo_proyecto(req, res) {
 }
 
 function nuevo_proyecto_alta(req, res) {
-
-    console.log(req.body);
+    const { nombre } = req.body;
+    // console.log(req.body);
     // res.status(200).json({mensaje:"formulario enviado"})
-    res.status(200).json({ cuerpo: req.body, estado: "OK", parametros: req.params });
+    let errores = [];
+    if (!nombre) {
+        errores.push({ 'texto': 'Necesario indicar nombre al Proyecto' });
+    }
+    if (errores.length > 0) {
+        res.render('nuevo_proyecto.pug', { nombrePagina: 'Nuevo Proyecto', errores });
+
+    } else {
+
+        Proyectos.create({ nombre })
+            .then(() => console.log('Insertado correctamente'))
+            .catch(error => console.log(error));
+        // res.status(200).json({ cuerpo: req.body, estado: "OK", parametros: req.params });
+    }
+
 }
 
 module.exports = { principal, nosotros, nuevo_proyecto, nuevo_proyecto_alta };

52b655f2e8eda9e5452266718e137db5622e80d7
Gomez Acin <juagomez@t-systems.es>
2020-02-13 23:46:10 +0100

    x1

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,18 @@ const express = require('express');
 const rutas = express.Router();
 
 const gralControl = require('../controlers/generalControler');
+// importar express validator
 
+const { body } = require('express-validator/check');
 
 
 rutas.get('/', gralControl.principal);
 rutas.get('/nosotros', gralControl.nosotros);
 rutas.get('/nuevo_proyecto', gralControl.nuevo_proyecto);
 
-rutas.post('/nuevo_proyecto', gralControl.nuevo_proyecto_alta);
+rutas.post('/nuevo_proyecto',
+    body('nombre').not().isEmpty().trim().escape(),
+    gralControl.nuevo_proyecto_alta);
 rutas.post('/formulario', (req, res) => { res.status(200).json({ valor: req.body }); });
 
 module.exports = { rutas };
